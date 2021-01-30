424c0e44f3b60fa1d0d34d9e028ab0a80fb65f70
skhirosh <skhirosh@gmail.com>
2021-01-30 02:17:49 -0500

    Add HUD stub

---
+++ _SuperSmashBanks/Assets/Scripts/HUDPanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HUDPanel : MonoBehaviour
+{
+    public Image panelImage;
+    public Image leftPanelImage;
+    public TextMeshProUGUI leftPanelTMPro;
+    public Image upperCenterPanelImage;
+    public TextMeshProUGUI upperCenterPanelTMPro;
+    public Image lowerCenterPanelImage;
+    public Image trendlineImage;
+    public Image rightPanelImage;
+    public TextMeshProUGUI rightPanelTMPro;
+
+    Color colorHudText = new Color32(0,255,0,255);
+    Color colorHudBackground = new Color32(0,0,0,255);
+    Color colorInvisible = new Color32(255,255,255,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetHud();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetHud() {
+        panelImage.color = colorHudBackground;
+        leftPanelTMPro.color = upperCenterPanelTMPro.color = rightPanelTMPro.color = colorHudText;
+        leftPanelImage.color = upperCenterPanelImage.color = lowerCenterPanelImage.color = rightPanelImage.color = colorInvisible;
+    }
+
+}
